4a80808beb08969af2a53ea12d640e9a672f2399
Tabook22 <nmtabook@gmail.com>
2016-08-01 20:15:18 +0400

    fixing products list pages

---
+++ OMXPro/Controllers/NewsListController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OMXPro.Models;
+namespace OMXPro.Controllers
+{
+    public class NewsListController : Controller
+    {
+        OmxtechDbContext db = new OmxtechDbContext();
+        // GET: NewsList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //News list 
+        public ActionResult siteNewsLst()
+        {
+            var getNewsLst = (from a in db.tbl_articles
+                                where a.a_loc == "10"
+                                orderby a.a_order ascending
+                                select a).ToList();
+
+            return PartialView("_NewsList", getNewsLst);
+        }
+
+        //News Sliding 
+        public ActionResult siteNewsSliding()
+        {
+            var getNewsSliding = (from a in db.tbl_articles
+                              where a.a_loc == "11"
+                              orderby a.a_order ascending
+                              select a).ToList();
+
+            return PartialView("_NewsSliding", getNewsSliding);
+        }
+    }
+}
---
+++ OMXPro/Controllers/ProductSlidingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OMXPro.Models;
+namespace OMXPro.Controllers
+{
+    public class ProductSlidingController : Controller
+    {
+        OmxtechDbContext db = new OmxtechDbContext();
+        // GET: ProductSliding
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //product sliding
+        public ActionResult productSliding()
+        {
+            var getProducts=(from a in db.tbl_articles
+                            where a.a_loc =="9"
+                            select a).ToList();
+
+            return PartialView("_ProductSliding", getProducts);
+        }
+    }
+}

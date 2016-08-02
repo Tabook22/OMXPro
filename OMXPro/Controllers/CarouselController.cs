1c593cb2177d9cefecdad5aaaa8b114443c9035e
nasser m. <nmtabook@gmail.com>
2016-08-02 19:33:44 +0400

    working of carousle

---
+++ OMXPro/Controllers/CarouselController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OMXPro.Models;
+namespace OMXPro.Controllers
+{
+    public class CarouselController : Controller
+    {
+        OmxtechDbContext db = new OmxtechDbContext();
+        // GET: Carousel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //site top carousle
+        public ActionResult siteCarousel()
+        {
+            var getSitCarousel = db.tbl_articles.Where(x => x.a_loc == "3").ToList();
+
+            return PartialView("_SiteCarousle", getSitCarousel);
+        }
+
+    }
+}
